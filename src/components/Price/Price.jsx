5ccae7247df336bf15f5d3b1a031c1786a929fde
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-12 20:31:56 -0400

    added a wrapper and inner div

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -9,39 +9,42 @@ const Price = ({
 }) => {
   return (
     <div className="checkout">
-      <div className="checkout__content">
-        <div className="checkout__title">
-          <h2 className="checkout__heading">Total Price:</h2>
-          <div className="checkout__prices">
-            <p className="checkout__price checkout__price--current">
-              {currentPrice}$/MON
-            </p>
-            <p className="checkout__price checkout__price--full">
-              {fullPrice}$/MON
-            </p>
+      <div className="checkout__inner">
+        <div className="checkout__content">
+          <div className="checkout__title">
+            <h2 className="checkout__heading">Total Price:</h2>
+            <div className="checkout__prices">
+              <p className="checkout__price checkout__price--current">
+                {currentPrice}$/MON
+              </p>
+              <p className="checkout__price checkout__price--full">
+                {fullPrice}$/MON
+              </p>
+            </div>
+          </div>
+          <div className="checkout__list">
+            {amountThemePacks && (
+              <p>
+                Added {amountThemePacks} theme{" "}
+                {amountThemePacks > 1 ? "packs" : "pack"}
+              </p>
+            )}
+            {amounChannels && (
+              <p>
+                Added {amounChannels}{" "}
+                {amounChannels > 1 ? "channels" : "channel"}
+              </p>
+            )}
+            {amountPremiums && (
+              <p>
+                Added {amountPremiums}{" "}
+                {amountPremiums > 1 ? "premiums" : "premium"}
+              </p>
+            )}
           </div>
         </div>
-        <div className="checkout__list">
-          {amountThemePacks && (
-            <p>
-              Added {amountThemePacks} theme{" "}
-              {amountThemePacks > 1 ? "packs" : "pack"}
-            </p>
-          )}
-          {amounChannels && (
-            <p>
-              Added {amounChannels} {amounChannels > 1 ? "channels" : "channel"}
-            </p>
-          )}
-          {amountPremiums && (
-            <p>
-              Added {amountPremiums}{" "}
-              {amountPremiums > 1 ? "premiums" : "premium"}
-            </p>
-          )}
-        </div>
+        <button className="checkout__button">Cart</button>
       </div>
-      <button className="checkout__button">Cart</button>
     </div>
   );
 };
