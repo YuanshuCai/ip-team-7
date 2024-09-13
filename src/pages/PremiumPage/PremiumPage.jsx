5a71810d6d955223ef9e68ed433da5229b91e4e4
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-12 23:42:20 -0700

    premium card list and price components added, missing search component

--- src/pages/PremiumPage/PremiumPage.jsx
+++ src/pages/PremiumPage/PremiumPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PremiumCardsList from '../../components/PremiumCardsList/PremiumCardsList'
+import Price from '../../components/Price/Price'
 import './PremiumPage.scss';
 
 function PremiumPage() {
   return (
     <div className='premium-page'>
         <PremiumCardsList />
+        <Price />
     </div>
   )
 }
