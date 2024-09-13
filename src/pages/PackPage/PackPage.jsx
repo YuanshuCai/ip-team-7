b93011779d697cccf0980e8afcdafbd927c8a785
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-13 12:15:55 -0400

    used preset in pack page

--- src/pages/PackPage/PackPage.jsx
+++ src/pages/PackPage/PackPage.jsx
@@ -1,14 +1,19 @@
 import Heading from "../../components/Heading/Heading";
 import CategoryCardList from "../../components/CategoryCardsList/CategoryCardsList";
+import PresetPacksDisplayButtons from "../../components/PresetPacksDisplayButtons/PresetPacksDisplayButtons";
 import "./PackPage.scss";
+import { useState } from "react";
 
 function PackPage({ selectedCategories, setSelectedCategories }) {
+  const [selectedChannels, setSelectedChannels] = useState([]);
   return (
     <div className="pack-page">
       <Heading text="theme packs" />
+      <PresetPacksDisplayButtons setSelectedChannels={setSelectedChannels} />
       <CategoryCardList
         selectedCategories={selectedCategories}
         setSelectedCategories={setSelectedCategories}
+        selectedChannels={selectedChannels}
       />
     </div>
   );
