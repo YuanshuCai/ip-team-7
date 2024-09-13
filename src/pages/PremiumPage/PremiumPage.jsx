96a05ecd5986214fadfc3c35cf6716c7810563b0
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-13 11:35:37 -0400

    added heading for a premium page

--- src/pages/PremiumPage/PremiumPage.jsx
+++ src/pages/PremiumPage/PremiumPage.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import PremiumCardsList from '../../components/PremiumCardsList/PremiumCardsList'
-import './PremiumPage.scss';
+import React from "react";
+import PremiumCardsList from "../../components/PremiumCardsList/PremiumCardsList";
+import Heading from "../../components/Heading/Heading";
+import "./PremiumPage.scss";
 
 function PremiumPage() {
   return (
-    <div className='premium-page'>
-        <PremiumCardsList />
+    <div className="premium-page">
+      <Heading text="premiums" />
+      <PremiumCardsList />
     </div>
-  )
+  );
 }
 
-export default PremiumPage
+export default PremiumPage;
