2d2ff3cf44efc9794b7d9bfbcfa6251dc4d4e3a6
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-12 23:05:48 -0400

    added settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.scss'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import "./styles/global.scss";
 
-createRoot(document.getElementById('root')).render(
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "fr"],
+    fallbackLng: "fr",
+    lng: "en",
+    debug: false,
+    detection: {
+      order: ["path", "cookie", "htmlTag"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/src/assets/data/{{lng}}/translation.json",
+    },
+  });
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
